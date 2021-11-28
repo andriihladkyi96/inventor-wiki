62eef8057ba7868be142270d39fda279a75dc506
andriihladkyi96 <andriihladkyi96@gmail.com>
2021-11-28 13:19:23 +0200

    fixed categories service

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -6,24 +6,24 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class CategoryService {
+export class CategoriesService {
 
-  categorysRef: AngularFireList<any>;
+  categoriesRef: AngularFireList<any>;
   categoryRef: AngularFireObject<any>;
-  private basePath: string = 'categorys';
+  private basePath: string = 'categories';
 
   constructor(private db: AngularFireDatabase) { }
 
   ngOnInit(): void {
-    this.categorysRef = this.db.list(this.basePath);
+    this.categoriesRef = this.db.list(this.basePath);
   }
 
   createCategory(category: Category):Promise<void> {
-    const newPostKey = this.categorysRef.push(category).key;
+    const newPostKey = this.categoriesRef.push(category).key;
     if (newPostKey) {
       category.id = newPostKey;
     }
-    return this.categorysRef.update(`/${newPostKey}`, category);
+    return this.categoriesRef.update(`/${newPostKey}`, category);
   }
 
   getCategory(id: string): Observable<Category> {
@@ -32,8 +32,8 @@ export class CategoryService {
   }
 
   getCategoryList(): Observable<Category[]> {
-    this.categorysRef = this.db.list(this.basePath);
-    return this.categorysRef.valueChanges();
+    this.categoriesRef = this.db.list(this.basePath);
+    return this.categoriesRef.valueChanges();
   }
 
   updateCategory(category: Category): Promise<void> {
@@ -43,7 +43,6 @@ export class CategoryService {
 
   deleteCategory(id: string): Promise<void> {
     this.getCategory(id);
-    debugger
     return this.categoryRef.remove();
   }
 }
